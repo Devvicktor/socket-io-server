223bb86d37b811e4dc8219d62ba1d16f914ab23f
Devvicktor <kosgeivick90@gmail.com>
2021-04-30 01:07:36 +0300

    make it neeter

---
+++ server.js
@@ -0,0 +1,30 @@
+const app = require("express")();
+const port=process.env.PORT || 3000;
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  // server-side
+  console.log("a user connected");
+  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+  socket.on("connect", () => {
+    console.log(socket.connected); // true
+  });
+  //send message
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+  //disconneted
+  socket.on("disconnect", () => {
+    console.log(socket.id); // undefined
+    console.log("user disconnected");
+  });
+});
+httpServer.listen(port, () => {
+  console.log("listening on *:3000");
+});
