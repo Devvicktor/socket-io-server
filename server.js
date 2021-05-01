81163a8bca9a5b5228134b41909c23607c798458
Devvicktor <kosgeivick90@gmail.com>
2021-05-01 11:09:38 +0300

    all over again

--- server.js
+++ server.js
@@ -1,36 +1,92 @@
-const app = require("express")();
-const cors=require('cors')
-const port=process.env.PORT || 3000;
-const httpServer = require("http").createServer(app);
-
-app.use(cors({
-  origin: "https://devvicktor.github.io"
-}))
-const io = require("socket.io")(httpServer, {
+const app = require ('express') ();
+const cors = require ('cors');
+const port = process.env.PORT || 3000;
+const httpServer = require ('http').createServer (app);
+const formatMessage = require ('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require ('./utils/users');
+app.use (
+  cors ({
+    origin: 'http://127.0.0.1:5500',
+  })
+);
+const io = require ('socket.io') (httpServer, {
   cors: {
-    origin: "https://devvicktor.github.io",
-    methods: ["GET", "POST"],
+    origin: 'http://127.0.0.1:5500',
+    methods: ['GET', 'POST'],
   },
-  transports : ['polling']
+  transports: ['polling'],
 });
-io.on("connection", (socket) => {
-  // server-side
-  console.log("a user connected");
-  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-  socket.on("connect", () => {
-    console.log(socket.connected); // true
+const botName = 'ZetiAlpha Bot';
+io.on ('connection', socket => {
+  /////////////////////////////
+  ///////// socket join toon
+  //////////////////////////////
+  socket.on ('joinRoom', ({username, room}) => {
+    //joined room
+    const user = userJoin (socket.id, username, room);
+    socket.join (user.room,`${user.username} joined room`);
+    console.log (user.room);
+    //welcome user to chat
+    socket.emit (
+      'chat message',
+      formatMessage (botName, 'Welcome to ChatCord!')
+    );
+    //broad cast when user connects
+    socket.broadcact
+      .to (user.room)
+      .emit (
+        'chat message',
+        formatMessage (botName, `${user.username} has joined the chat`)
+      );
+
+    //get room users
+    // Send users and room info
+    io.to (user.room).emit ('roomUsers', {
+      room: user.room,
+      users: getRoomUsers (user.room),
+    });
   });
-  //send message
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-    io.emit("chat message", msg);
+  ///////////////////////////////////
+  /////////lets send data across
+  /////////////////////////////////////
+  socket.on ('chat message', msg => {
+    console.log ('message: ' + msg);
+    const user = getCurrentUser (socket.id);
+    console.log ('user', user);
+    console.log ('socketid', socket.id);
+    io.to (user.room).emit ('chat message', formatMessage (user.username, msg));
   });
-  //disconneted
-  socket.on("disconnect", () => {
-    console.log(socket.id); // undefined
-    console.log("user disconnected");
+  ///////////////////////
+  ////////leave room
+  ///////////////////////
+  // Runs when client disconnects
+  socket.on ('disconnect', () => {
+    const user = userLeave (socket.id);
+
+    if (user) {
+      io
+        .to (user.room)
+        .emit (
+          'chat message',
+          formatMessage (botName, `${user.username} has left the chat`)
+        );
+
+      // Send users and room info
+      io.to (user.room).emit ('roomUsers', {
+        room: user.room,
+        users: getRoomUsers (user.room),
+      });
+    }
   });
 });
-httpServer.listen(port, () => {
-  console.log("listening on *:3000");
+//////////////////////////
+//////////serving
+///////////////////////////
+httpServer.listen (port, () => {
+  console.log ('listening on *:3000');
 });
